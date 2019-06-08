830fb2cb8e0e8cfef0d64f0483d91e7d5824c67e
Fairness2 <geve95@gmail.com>
2019-06-08 16:19:54 +0500

    Начало отчётов

--- server/app/Http/Controllers/ReportsController.php
+++ server/app/Http/Controllers/ReportsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ReportsController extends Controller
 {
@@ -54,5 +56,54 @@ class ReportsController extends Controller
         foreach ($workers as $worker){
 
         }
+    }
+
+    public function individualReport(Request $request){
+        $allotmentId = $request->get('allotment', null);
+        $workerId = $request->get('worker', null);
+
+        $validator = Validator::make(['allotment_id' => $allotmentId, 'worker_id' => $workerId],[
+           'allotment_id' => ['required', 'integer', Rule::exists('allotment')->where(function ($query){
+               $query->where('is_remove', '<>', true);
+           })],
+            'worker_id' => ['required', 'integer', Rule::exists('worker')->where(function ($query){
+                $query->where('not_active', '<>', true);
+            })]
+        ]);
+        if ($validator->fails())
+            abort(404);
+
+        $worker = DB::table('worker')->where('id', $workerId)->get()->first();
+
+        $loads = DB::table('load_element')
+            ->leftJoin('distribution_element', 'load_element.id', '=', 'distribution_element.load_element_id')
+            ->leftJoin('discipline', 'load_element.discipline_id', '=', 'discipline.id')
+            ->leftJoin('group', 'load_element.group_id', '=', 'group.id')
+            ->leftJoin('type_class', 'load_element.type_class_id', '=', 'type_class.id')
+            ->leftJoin('specialty', 'group.specialty_id', '=', 'specialty.id')
+            ->leftJoin('faculty', 'specialty.faculty_id', '=', 'faculty.id')
+            ->where([['load_element.allotment_id', $allotmentId], ['distribution_element.worker_id', $workerId]])
+            ->select([
+                'discipline.name AS discipline',
+                'faculty.name AS faculty',
+                'group.name AS group',
+                'type_class.name AS type_class',
+                'load_element.hours_planed AS hours',
+                'load_element.semester AS semester'
+            ])
+            ->get()->all();
+
+        foreach ($loads as &$load) {
+            $anotherWorkers = DB::table('load_element')
+                ->leftJoin('distribution_element', 'load_element.id', '=', 'distribution_element.load_element_id')
+                ->leftJoin('worker');
+        }
+        $autumnLoads = array_filter($loads, function ($item) {
+            return $item->semester == 1;
+        });
+        $springLoads = array_filter($loads, function ($item) {
+            return $item->semester == 2;
+        });
+
     }
 }
